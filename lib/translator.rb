cfb5b32a84f7d72db4a4690cdedbd030e267e5d6
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-04 20:50:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,14 @@ def load_library(filename)
   emoticon_data = {'get_meaning' => {}, 'get_emoticon'=> {}
   }
   files.each do |key, value| 
+    emoticon_data['get_meaning'][value[1]] = key 
+    emoticon_data['get_emoticon'][value[0]] = files[key][1]
   # code goes here
+end 
+return emoticon_data
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon()
   # code goes here
 end
 
